10ae227322268931f1d2a182eb22d8481d3ce645
ninebelowzero <richardjohnsilk@gmail.com>
2015-10-12 11:57:22 +0100

    Now logs shaded squares in array.

--- tablut/app.js
+++ tablut/app.js
@@ -3,92 +3,367 @@ $(function(){
 })
 
 
-var board = {
-	blackToMove: true,
+var tablut = {
+	board: [["edge", "edge", "edge", "edge", "edge", "edge", "edge", "edge", "edge", "edge", "edge"],
+	["edge", null, null, null, "B", "B", "B", null, null, null, "edge"],
+	["edge", null, null, null, null, "B", null, null, null, null, "edge"],
+	["edge", null, null, null, null, "W", null, null, null, null, "edge"],
+	["edge", "B", null, null, null, "W", null, null, null, "B", "edge"],
+	["edge", "B", "B", "W", "W", "K", "W", "W", "B", "B", "edge"],
+	["edge", "B", null, null, null, "W", null, null, null, "B", "edge"],
+	["edge", null, null, null, null, "W", null, null, null, null, "edge"],
+	["edge", null, null, null, null, "B", null, null, null, null, "edge"],
+	["edge", null, null, null, "B", "B", "B", null, null, null, "edge"],
+	["edge", "edge", "edge", "edge", "edge", "edge", "edge", "edge", "edge", "edge", "edge"]],
+	homeColor: "B",
+	enemyColor: "W",
 	highlighted: false,
-	movingFrom: null,
+	$leaving: null,
 	pieceSelected: {
-		row: null,
-		column: null,
+//		row: null,
+//		column: null,
 		letter: null
 	},
+	movingFrom: [null, null],
+	moveDirection: null,
 	movingTo: {
 		row: null,
 		column: null
 	},
-	kingMoved: false
+	kingIsAt: {
+		row: 5,
+		column: 5
+	},
+	gameOver: false
 };
-
-
-function legalMove(square){
-	if (square.text() === "B" && board.blackToMove) return true;
-	if (square.text() === "W" && !board.blackToMove) return true;
-	if (square.text() === "K" && !board.blackToMove){
-		board.kingMoved = true;
-		return true;
-	}
-}
-
-
-function newMove() {
-	board.movingFrom.text("");
-	board.movingFrom.toggleClass("highlighted");
-	board.highlighted = null;
-	board.movingFrom = null;
-	board.pieceSelected.row = null;
-	board.pieceSelected.column = null;
-	board.pieceSelected.letter = null;
-	board.movingTo.row = null;
-	board.movingTo.column = null;
-	board.blackToMove = !board.blackToMove;
-	if (board.blackToMove) {
-		$(".status-text").text("Black to move.");
-	} else {
-		$(".status-text").text("White to move.");					
-	}
-	board.kingMoved = false;
-}
 
 
 function listenForClicks(){
 	$("td").on("click", function(){
 		var square = $(this);
-		if (!board.highlighted){
-			if (legalMove(square)){
-				highlight(square);
-			}
-		} else if (board.highlighted){
-			if (board.pieceSelected.row === square.attr("data-row") && board.pieceSelected.column === square.attr("data-column")) {
-				square.toggleClass("highlighted");
-				board.highlighted = false;
-			} else if (square.text()){
-				console.log("Square occupied");
-			} else {
-				movePieceTo(square);
-				newMove();
-			}
-		}
+		clickedOn(square);
 	})
 }
 
 
-function highlight(square){
-	board.pieceSelected.row = square.attr("data-row");
-	board.pieceSelected.column = square.attr("data-column");
-	board.pieceSelected.letter = square.text();
-	square.toggleClass("highlighted");
-	board.highlighted = true;
-	console.log("Selected " + board.pieceSelected.letter + " at " + board.pieceSelected.row + ", " + board.pieceSelected.column);
-	board.movingFrom = square;	
+function clickedOn(square){
+	if (!tablut.gameOver){
+		if (!tablut.highlighted){
+			selectingPiece(square);
+		} else {
+			clickedAfterHighlighting(square);
+		}
+	}
 }
+
+
+function selectingPiece(square){
+	if (legalSelection(square)){
+		highlight(square);
+	}
+}
+
+
+function legalSelection(square){
+	if (!square.text()) return false;
+	if (square.text() === tablut.homeColor) return true;
+	if (square.text() === "K" && tablut.homeColor === "W") return true;
+}
+
+
+function highlight(square){
+	// tablut.pieceSelected.row = parseInt(square.attr("data-row"));
+	// tablut.pieceSelected.column = parseInt(square.attr("data-column"));
+
+	tablut.movingFrom[0] = parseInt(square.attr("data-row"));
+	tablut.movingFrom[1] = parseInt(square.attr("data-column"));
+
+	tablut.pieceSelected.letter = square.text();
+	square.toggleClass("highlighted");
+	tablut.highlighted = true;
+	console.log("Selected " + tablut.pieceSelected.letter + " at " + tablut.movingFrom);
+	tablut.$leaving = square;	
+}
+
+
+function unhighlight(square){
+	square.toggleClass("highlighted");
+	tablut.highlighted = false;
+}
+
+
+function clickedAfterHighlighting(square){
+	if (identical(square)){
+		unhighlight(square);
+	} else {
+		attemptMove(square);
+	}
+}
+
+
+function identical(square){
+	if (tablut.movingFrom[0] == square.attr("data-row") && tablut.movingFrom[1] == square.attr("data-column")) {
+		return true;
+	} 
+}
+
+
+function attemptMove(square){
+	if (legalMove(square)){
+		movePieceTo(square);
+		wipeVacated();
+		checkForCaptures(square);
+		checkForWins();
+	} else {
+		console.log("Illegal move.")
+	}
+}
+
+
+function legalMove(square){
+	if (available(square) && matchesGridPosition(square) && noInterveningPieces(square)){
+		return true;
+	}
+}
+
+
+function available(square){
+	if (square.text()){
+		console.log("Square occupied");
+		return false;
+	} else if (square.hasClass("shaded")){
+		console.log("Cannot move to shaded squares.");
+		return false;
+	} else {
+		return true;
+	}
+}
+
+
+function matchesGridPosition(square){
+	if (square.attr("data-row") == tablut.movingFrom[0]){
+		tablut.moveDirection = "horizontal";
+		return true;
+	} else if (square.attr("data-column") == tablut.movingFrom[1]){
+		tablut.moveDirection = "vertical";
+		return true;
+	}
+}
+
+
+// BELOW SECTION V WET, NEEDS REFACTORING
+
+function noInterveningPieces(square){	
+	if (tablut.moveDirection === "horizontal"){
+		return horizontalMove(square);
+	} else if (tablut.moveDirection === "vertical"){
+		return verticalMove(square);
+	}
+}
+
+
+function horizontalMove(square) {
+
+	var comingFromColumn = tablut.movingFrom[1];
+	var goingToColumn = square.attr("data-column");
+
+	if (goingToColumn > comingFromColumn){
+		for (var i = comingFromColumn + 1; i < goingToColumn; i++){
+			var intermediateSquare = tablut.board[tablut.movingFrom[0]][i];
+			if (intermediateSquare !== null){
+				console.log("There are intervening pieces.")
+				return false;
+			}
+		}
+		return true;
+
+	} else if (comingFromColumn > goingToColumn){
+		for (var i = comingFromColumn - 1; i > goingToColumn; i--){
+			var intermediateSquare = tablut.board[tablut.movingFrom[0]][i];
+			if (intermediateSquare !== null){
+				console.log("There are intervening pieces.")
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+
+function verticalMove(square){
+	
+	var comingFromRow = tablut.movingFrom[0];
+	var goingToRow = square.attr("data-row");
+
+	if (goingToRow > comingFromRow){
+		for (var i = comingFromRow + 1; i < goingToRow; i++){
+			var intermediateSquare = tablut.board[i][tablut.movingFrom[1]];
+			if (intermediateSquare !== null){
+				console.log("There are intervening pieces.")
+				return false;
+			}
+		}
+		return true;
+
+	} else if (comingFromRow > goingToRow){
+		for (var i = comingFromRow - 1; i > goingToRow; i--){
+			var intermediateSquare = tablut.board[i][tablut.movingFrom[1]];
+			if (intermediateSquare !== null){
+				console.log("There are intervening pieces.")
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+// END OF WET CODE
 
 
 function movePieceTo(square){
-	board.movingTo.row = square.attr("data-row");
-	board.movingTo.column = square.attr("data-column");
-	console.log("Moving to: " + board.movingTo.row + ", " + board.movingTo.column);
-	square.text(board.pieceSelected.letter);
-	// Would like to highlight destination square - but needs a timer.
-	// square.toggleClass("highlighted");
+	tablut.movingTo.row = parseInt(square.attr("data-row"));
+	tablut.movingTo.column = parseInt(square.attr("data-column"));
+	console.log("Moving to: " + tablut.movingTo.row + ", " + tablut.movingTo.column);
+	square.text(tablut.pieceSelected.letter);
+	tablut.board[tablut.movingTo.row][tablut.movingTo.column] = tablut.pieceSelected.letter;
+	if (tablut.pieceSelected.letter === "K"){
+		tablut.kingIsAt.row = tablut.movingTo.row;
+		tablut.kingIsAt.column = tablut.movingTo.column;
+	}
 }
 
+
+// MORE WET CODE
+
+function checkForCaptures(square){
+	lookNorth();
+	lookEast();
+	lookSouth();
+	lookWest();
+}
+
+function lookNorth(){
+	if (tablut.board[tablut.movingTo.row - 1][tablut.movingTo.column] === tablut.enemyColor){
+		checkingSquare = $("tr:nth-child(" + (tablut.movingTo.row - 1) + ") td:nth-child(" + tablut.movingTo.column + ")");
+		if (tablut.board[tablut.movingTo.row - 2][tablut.movingTo.column] === tablut.homeColor){
+			tablut.board[tablut.movingTo.row - 1][tablut.movingTo.column] = null;
+			checkingSquare.text("");
+		}
+	} 
+}
+
+function lookEast(){
+	if (tablut.board[tablut.movingTo.row][tablut.movingTo.column + 1] === tablut.enemyColor){
+		checkingSquare = $("tr:nth-child(" + tablut.movingTo.row + ") td:nth-child(" + (tablut.movingTo.column + 1) + ")");
+		if (tablut.board[tablut.movingTo.row][tablut.movingTo.column + 2] === tablut.homeColor){
+			tablut.board[tablut.movingTo.row][tablut.movingTo.column + 1] = null;
+			checkingSquare.text("");
+		}
+	}
+}
+
+function lookSouth(){
+	if (tablut.board[tablut.movingTo.row + 1][tablut.movingTo.column] === tablut.enemyColor){
+		checkingSquare = $("tr:nth-child(" + (tablut.movingTo.row + 1) + ") td:nth-child(" + tablut.movingTo.column + ")");
+		if (tablut.board[tablut.movingTo.row + 2][tablut.movingTo.column] === tablut.homeColor){
+			tablut.board[tablut.movingTo.row + 1][tablut.movingTo.column] = null;
+			checkingSquare.text("");
+		}
+	}
+}
+
+function lookWest(){
+	if (tablut.board[tablut.movingTo.row][tablut.movingTo.column - 1] === tablut.enemyColor){
+		checkingSquare = $("tr:nth-child(" + tablut.movingTo.row + ") td:nth-child(" + (tablut.movingTo.column - 1) + ")");
+		if (tablut.board[tablut.movingTo.row][tablut.movingTo.column - 2] === tablut.homeColor){
+			tablut.board[tablut.movingTo.row][tablut.movingTo.column - 1] = null;
+			checkingSquare.text("");
+		}
+	}
+}
+
+// END OF WET CODE
+
+
+function wipeVacated(){
+	tablut.$leaving.text("");
+	tablut.$leaving.toggleClass("highlighted");
+	if (leavingShadedSquare() ){
+		tablut.board[tablut.movingFrom[0]][tablut.movingFrom[1]] = "shaded";
+	} else {
+		tablut.board[tablut.movingFrom[0]][tablut.movingFrom[1]] = null;
+	}
+	tablut.highlighted = null;
+	tablut.$leaving = null;
+	// tablut.pieceSelected.row = null;
+	// tablut.pieceSelected.column = null;
+	tablut.pieceSelected.letter = null;
+}
+
+
+function leavingShadedSquare(){
+	if (tablut.movingFrom.toString() === "1,4" || "1,5" || "1,6" || "2,5" || "4,1" || "4,9" || "5,1" || "5,2" || "5,5" || "5,8" || "5,9" || "6,1" || "6,9" || "8,5" || "9,4" || "9,5" || "9,6"){
+		console.log("Leaving shaded square.")
+		return true;
+	}
+}
+
+
+function checkForWins(){
+	checkForWhiteWin();
+	checkForBlackWin();
+	if (!tablut.gameOver){
+		switchTurns();
+	}
+}
+
+
+function switchTurns(){
+	tablut.moveDirection = null;
+	tablut.movingTo.row = null;
+	tablut.movingTo.column = null;
+	if (tablut.homeColor === "B"){
+		tablut.homeColor = "W";
+		tablut.enemyColor = "B";
+	} else {
+		tablut.homeColor = "B";
+		tablut.enemyColor = "W";
+	}
+	if (tablut.homeColor === "B"){
+		$(".status-text").text("Black to move.");
+	} else {
+		$(".status-text").text("White to move.");	
+	}
+}
+
+
+function checkForWhiteWin(){
+	if (tablut.kingIsAt.row === 1 || tablut.kingIsAt.row === 9 || tablut.kingIsAt.column === 1 || tablut.kingIsAt.column === 9) {
+		$(".status-text").text("The King escapes! White wins the game.");
+		tablut.gameOver = true;
+	}
+}
+
+
+function checkForBlackWin(){
+	if (kingTrappedFromNorth() && kingTrappedFromEast() && kingTrappedFromWest() && kingTrappedFromSouth()){
+			$(".status-text").text("The King is surrounded! Black wins the game.");	
+			tablut.gameOver = true;	
+	}
+}
+
+function kingTrappedFromNorth(){
+	if (tablut.board[tablut.kingIsAt.row][tablut.kingIsAt.column - 1] === "B") return true;
+}
+
+function kingTrappedFromEast(){
+	if (tablut.board[tablut.kingIsAt.row + 1][tablut.kingIsAt.column] === "B") return true;
+}
+
+function kingTrappedFromSouth(){
+	if (tablut.board[tablut.kingIsAt.row][tablut.kingIsAt.column + 1] === "B") return true;
+}
+
+function kingTrappedFromWest(){
+	if (tablut.board[tablut.kingIsAt.row - 1][tablut.kingIsAt.column] === "B") return true;
+}
